28be6d30283923693434c4004f8a30d460887ca4
BakaZoro <56846056+BakaZoro@users.noreply.github.com>
2019-11-26 01:02:51 +0530

    Add files via upload

---
+++ DoorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+	Animator ani;
+
+	// Use this for initialization
+	void Start () {
+
+		ani = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+	
+		ani.SetTrigger ("OpenDoor");
+	
+	}
+
+	void OnTriggerExit(Collider other){
+
+		ani.enabled = true;
+
+	}
+
+	void pauseAnimation(){
+
+		ani.enabled = false;
+	}
+}
